c8727520840918f7d1c657532482dd3be8817eaa
pierrebruninmaif <pierre.brunin@maif.fr>
2019-05-14 20:22:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+function opunMiddleware(req, res, next) {
+    console.log('set header', req.get('Opun-Gateway-State'));
+    res.set('Opun-Gateway-State-Resp', req.get('Opun-Gateway-State') || 'none');
+    return next();
+}
+
+app.use(opunMiddleware);
+
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
